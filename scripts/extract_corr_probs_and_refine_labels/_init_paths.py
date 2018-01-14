7f9e43e849d383faff99173c8b772fc318f478cb
zyf <zhaoyafei0210@gmail.com>
2018-01-15 02:19:37 +0800

    add caffe path setting into _init_paths.py

--- scripts/extract_corr_probs_and_refine_labels/_init_paths.py
+++ scripts/extract_corr_probs_and_refine_labels/_init_paths.py
@@ -2,3 +2,7 @@ import sys
 import os.path as osp
 
 sys.path.append(osp.join(osp.dirname(__file__), '../..'))
+
+# set caffe path
+caffe_root = '/opt/caffe/'
+sys.path.insert(0, caffe_root + 'python')
