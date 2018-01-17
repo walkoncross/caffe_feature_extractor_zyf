1a692ccd1ef5099606511631c4d5df14757d8ab8
zyf <zhaoyafei0210@gmail.com>
2018-01-17 21:08:49 +0800

    edit batch size

--- scripts/extract_and_save_probs_stats/extract_and_save_probs_stats.py
+++ scripts/extract_and_save_probs_stats/extract_and_save_probs_stats.py
@@ -263,6 +263,8 @@ def extract_and_save_probs_stats(config_json, max_orig_label,
     print('\n===> Saving features stats (avg, std) for each id under ' + save_dir)
     save_feats_stats_dict(feats_stats_dict, save_dir)
 
+    print('\n===> Saving save_max_label_info_stats under ' + save_dir)
+
     save_max_label_info_stats(
         cnt_per_id_vec, feats_stats_dict, save_dir, is_train_set)
 
