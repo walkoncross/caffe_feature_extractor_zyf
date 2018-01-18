d21992c178bef09382826a0518a72a95ff048390
zyf <zhaoyafei0210@gmail.com>
2018-01-18 08:07:22 +0800

    fix img dir in scripts/extract_corr_probs_and_refine_labels/test_scripts/test_msceleb_probs_on_sphere64_msceleb.py

--- scripts/extract_corr_probs_and_refine_labels/test_scripts/test_msceleb_probs_on_sphere64_msceleb.py
+++ scripts/extract_corr_probs_and_refine_labels/test_scripts/test_msceleb_probs_on_sphere64_msceleb.py
@@ -15,12 +15,12 @@ if __name__ == '__main__':
         config_json['gpu_id'] = int(sys.argv[1])
 
     prob_thresh = 0.55
-    first_new_id = 78771
-    max_orig_label = 10245
+    first_new_id = 0
+    max_orig_label = 78770
 
     # image path: osp.join(image_dir, <each line in image_list_file>)
-    image_dir = r'/disk2/data/FACE/face-asian/face_asian_align/'
-    image_list_file = r'/disk2/zhaoyafei/face-recog-train/train-val-lists/asian/face_asian_train_list_noval_10245-ids_540735-objs_170818-225846-norootdir.txt'
+    image_dir = r"/disk2/data/FACE/celeb-1m-mtcnn-aligned/msceleb_align/Faces-Aligned/"
+    image_list_file = r'/disk2/zhaoyafei/face-recog-train/train-val-lists/msceleb-1m/MS-Celeb-1M_clean_list_fixed2_78771_ids_5048805_imgs.txt'
 
     save_dir = '../../prob-results/asian_probs_on_sphere64_msceleb'
     num_images = -1 # <0, means all images
