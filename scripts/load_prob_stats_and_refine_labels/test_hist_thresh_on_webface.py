182105b716912aa96f857250d5af4fdb6a153286
zyf <zhaoyafei0210@gmail.com>
2018-01-19 20:34:19 +0800

    rename test_hist_thresh.py into test_hist_thresh_on_webface.py

--- scripts/load_prob_stats_and_refine_labels/test_hist_thresh_on_webface.py
+++ scripts/load_prob_stats_and_refine_labels/test_hist_thresh_on_webface.py
@@ -3,36 +3,32 @@
 from load_prob_stats_and_calc_hist_thresh import load_prob_stats_and_calc_hist_thresh
 
 if __name__ == '__main__':
-    stats_fn1_list = [r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-webface-webface-corr.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-webface-webface-softmax.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-webface-msceleb-corr.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-webface-msceleb-softmax.txt'
-                      ]
+    stats_fn1_list = [
+        r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-webface-webface-corr.txt',
+        r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-webface-webface-softmax.txt'
+    ]
     num_ids1 = 10572
     num_images1 = -1
 
-    stats_fn2_list = [r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-asian-webface-corr.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-asian-webface-softmax.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-asian-msceleb-corr.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-asian-msceleb-softmax.txt'
-                      ]
+    stats_fn2_list = [
+        r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-asian-webface-corr.txt',
+        r'C:\zyf\dnn_models\face_models\face-datasets-merge\stats-max-label-info-asian-webface-softmax.txt'
+    ]
     num_ids2 = 10245
     num_images2 = -1
 
-    stats_fn3_list = [r'C:\zyf\dnn_models\face_models\face-datasets-merge\corr_prob-stats-max-label-info-vggface-webface.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\softmax_prob-stats-max-label-info-vggface-webface.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\corr_prob-stats-max-label-info-vggface-msceleb.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\softmax_prob-stats-max-label-info-vggface-msceleb.txt'
-                      ]
+    stats_fn3_list = [
+        r'C:\zyf\dnn_models\face_models\face-datasets-merge\corr_prob-stats-max-label-info-vggface-webface.txt',
+        r'C:\zyf\dnn_models\face_models\face-datasets-merge\softmax_prob-stats-max-label-info-vggface-webface.txt'
+    ]
     num_ids3 = 2564
     num_images3 = -1
 
-    stats_fn4_list = [r'C:\zyf\dnn_models\face_models\face-datasets-merge\corr_prob-stats-max-label-info-vggface2-webface.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\softmax_prob-stats-max-label-info-vggface2-webface.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\corr_prob-stats-max-label-info-vggface2-msceleb.txt',
-                      r'C:\zyf\dnn_models\face_models\face-datasets-merge\softmax_prob-stats-max-label-info-vggface2-msceleb.txt'
-                      ]
-    num_ids4 = 2564
+    stats_fn4_list = [
+        r'C:\zyf\dnn_models\face_models\face-datasets-merge\corr_prob-stats-max-label-info-vggface2-webface.txt',
+        r'C:\zyf\dnn_models\face_models\face-datasets-merge\softmax_prob-stats-max-label-info-vggface2-webface.txt'
+    ]
+    num_ids4 = 8631
     num_images4 = -1
 
 #    only_after_bin_val = False
