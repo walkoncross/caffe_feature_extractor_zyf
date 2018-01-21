e1c30821a097be73dc4253c6e2d002d6f0f80417
zyf <zhaoyafei0210@gmail.com>
2018-01-21 08:07:20 +0800

    add scripts/test_and_plot_probs_hists/test_avg_fc5_corr/*

---
+++ scripts/test_and_plot_probs_hists/test_avg_fc5_corr/test_hist_thresh_corr_on_msceleb.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import _init_paths
+import os
+import os.path as osp
+from load_prob_stats_and_calc_hist_thresh import load_prob_stats_and_calc_hist_thresh
+
+if __name__ == '__main__':
+    root_dir = r'C:\zyf\dnn_models\face_models\face-datasets-merge\inter_datasets_avg_fc5_feats_corr_msceleb'
+    file_list = os.listdir(root_dir)
+
+    stats_fn_list = [osp.join(root_dir, fn) for fn in file_list]
+    # num_ids_list = [78771, 10572, 10245, 2564, 8631]
+
+    num_ids_list = []
+    for fn in file_list:
+        if fn.startswith('corr_mat-asian'):
+            num_ids_list.append(10245)
+        elif fn.startswith('corr_mat-webface'):
+            num_ids_list.append(10572)
+        elif fn.startswith('corr_mat-vggface2'):
+            num_ids_list.append(8631)
+        elif fn.startswith('corr_mat-vggface-'):
+            num_ids_list.append(2564)
+        elif fn.startswith('corr_mat-msceleb'):
+            num_ids_list.append(78771)
+        else:
+            raise Exception('Unkonw dataset')
+
+    num_images_list = None
+
+
+# #    only_after_bin_val = False
+#     only_after_bin_val = 0.55
+# #    only_after_bin_val = 0.5
+    only_after_bin_val_list = [0, 0.55, 0.5]
+
+    show_hist = False
+
+    num_fns = len(stats_fn_list)
+
+    save_root_dir = './rlt_avg_fc5_corr_hist/feat_hist_on_msceleb'
+
+    for bin_val in only_after_bin_val_list:
+        save_dir = osp.join(save_root_dir, 'hist_png')
+        if bin_val > 0:
+            save_dir += '_thr_%g' % bin_val
+
+        for i, stats_fn in enumerate(stats_fn_list):
+                num_images1 = -1
+                if num_ids_list:
+                    num_images1 = num_ids_list[i]
+                load_prob_stats_and_calc_hist_thresh(stats_fn_list[i], num_ids_list[i], num_images1,
+                                                     None, None, None,
+                                                     bin_val, show_hist, save_dir)
