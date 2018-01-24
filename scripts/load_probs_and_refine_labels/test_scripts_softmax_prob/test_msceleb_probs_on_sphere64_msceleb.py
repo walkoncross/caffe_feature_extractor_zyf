68dfb87e598cc38f21adf48718236c9107ef0461
zyf <zhaoyafei0210@gmail.com>
2018-01-25 01:20:09 +0800

    add load_probs_and_refine_labels for msceleb

---
+++ scripts/load_probs_and_refine_labels/test_scripts_softmax_prob/test_msceleb_probs_on_sphere64_msceleb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import _init_paths
+from load_probs_and_refine_labels import load_probs_and_refine_labels
+import os.path as osp
+import numpy as np
+
+if __name__ == '__main__':
+
+    prob_threshs = np.arange(0.615, 0.7, 0.75, 0.8)
+    first_new_id = 0
+    max_orig_label = 78770
+
+    # image path: osp.join(image_dir, <each line in image_list_file>)
+    prob_dir = r'/home/msceleb_probs_on_sphere64_msceleb/prob'
+    image_list_file = r'/disk2/zhaoyafei/face-recog-train/train-val-lists/msceleb-1m/MS-Celeb-1M_clean_list_fixed2_78771_ids_5048805_imgs.txt'
+
+    # save_dir = None
+    save_dir = '../../prob-results/msceleb_probs_on_sphere64_msceleb/softmax_prob_threshed_results'
+
+    num_images = -1  # <0, means all images
+    mirror_input = False
+
+    for thresh in prob_threshs:
+        load_probs_and_refine_labels(prob_dir, thresh,
+                                     first_new_id, max_orig_label,
+                                     image_list_file, num_images,
+                                     save_dir)
