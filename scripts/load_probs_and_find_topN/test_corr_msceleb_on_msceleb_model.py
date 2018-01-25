d12e02533b642408555908a3a788c8c8f3f66208
zyf <zhaoyafei0210@gmail.com>
2018-01-25 16:29:48 +0800

    add scripts/load_probs_and_find_topN/test_corr_msceleb_on_msceleb_model.py

---
+++ scripts/load_probs_and_find_topN/test_corr_msceleb_on_msceleb_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from load_probs_and_find_topN import load_probs_and_find_topN
+
+if __name__ == '__main__':
+
+    prob_dir = '/home/msceleb_probs_on_sphere64_msceleb/corr_prob'
+    image_list_file = r'/disk2/zhaoyafei/face-recog-train/train-val-lists/msceleb-1m/MS-Celeb-1M_clean_list_fixed2_78771_ids_5048805_imgs.txt'
+
+    num_ids = 78771
+    num_images = -1
+    top_n = 10
+    save_dir = '../prob-results/msceleb_probs_on_sphere64_msceleb/rlt_top_%d_corr_prob_per_id_img_list' % top_n
+
+    load_probs_and_find_topN(prob_dir, num_ids,
+                             image_list_file, num_images,
+                             save_dir, top_n)
