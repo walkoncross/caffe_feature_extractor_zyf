d37f60e5c467d840718382f7049a3f6e293a566e
zyf <zhaoyafei0210@gmail.com>
2018-02-03 04:40:34 +0800

    add extract_features_for_fgnet.py

---
+++ scripts/extract_features_for_megaface/sphere64-ms-fixed2-0107-kirk/extract_features_for_fgnet.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+import _init_paths
+import sys
+from extract_features_fgnet import extract_features
+
+
+if __name__ == '__main__':
+    config_json = './extractor_config_sphere64_pod.json'
+    save_dir = '/disk2/zhaoyafei/megaface-eval/features/sphereface-64-ms-fixed2-0107/FGNet_SPF512Features'
+    gpu_id = None
+
+    if len(sys.argv) > 1:
+        gpu_id = int(sys.argv[1])
+
+    # image path: osp.join(image_dir, <each line in image_list_file>)
+    image_dir = r'/disk2/data/FACE/megaface/fgnet_mtcnn_aligned/aligned_imgs'
+    image_list_file = r'/disk2/data/FACE/megaface/face_aligner/fgnet_mtcnn_aligned/fgnet-mtcnn-aligned-image-list.txt.txt'
+    extract_features(config_json, save_dir, image_list_file, image_dir, gpu_id=gpu_id)
